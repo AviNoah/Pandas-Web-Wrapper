5826145cbecd04cc5d03e90d937204e42bb302ec
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-08 12:19:55 +0200

    Added index route and upload_file route, upload_file will execute once a request with a file is sent from the user agent and will check if ti an allowed file using is_allowed_file, then it will save it into a temp folder

--- app.py
+++ app.py
@@ -13,3 +13,24 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def is_allowed_file(filename) -> bool:
     _, ext = os.path.splitext(filename)
     return ext in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file in request'
+    
+    file = request.files['file']
+    
+    if not file or file.filename == "":
+        return "No file selected"
+    
+    if not is_allowed_file(file):
+        return "Invalid file type"
+    
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+    return "Uploaded file successfully"
