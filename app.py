1ae53ff86fba0e4c20e7f332a3e543b9c4356aec
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-08 12:27:41 +0200

    Added templates folder and renamed process_files method to show_files, renamed its route from show_files to process_files

--- app.py
+++ app.py
@@ -6,43 +6,48 @@ import tempfile
 app = Flask(__name__)
 
 UPLOAD_FOLDER: str = tempfile.mkdtemp()
-ALLOWED_EXTENSIONS: set = {'.xlsx', '.csv'}
+ALLOWED_EXTENSIONS: set = {".xlsx", ".csv"}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 def is_allowed_file(filename) -> bool:
     _, ext = os.path.splitext(filename)
     return ext in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/upload', methods=["POST"])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        return 'No file in request'
-    
-    file = request.files['file']
-    
+    if "file" not in request.files:
+        return "No file in request"
+
+    file = request.files["file"]
+
     if not file or file.filename == "":
         return "No file selected"
-    
+
     if not is_allowed_file(file):
         return "Invalid file type"
-    
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
     file.save(file_path)
     return "Uploaded file successfully"
 
-@app.route('/show_files')
-def process_files():
+
+@app.route("/process_files")
+def show_files():
     # Show the files
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    files = os.listdir(app.config["UPLOAD_FOLDER"])
     return f"Files in temporary folder: {files}"
 
-@app.route('/discard_files')
+
+@app.route("/discard_files")
 def discard_session():
     # Remove all files from temp folder, start new session
-    shutil.rmtree(app.config['UPLOAD_FOLDER'])
+    shutil.rmtree(app.config["UPLOAD_FOLDER"])
     return "Session dropped, files removed from temporary folder"
