c832264f6987ab5d53350cc89fc666b14b0de465
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-09 16:27:33 +0200

    Added a check to see if a fileListContainer exists, moved images to static instead of in javascripts, removed notification display scipt form files.html

--- static/javascripts/fileDisplay.js
+++ static/javascripts/fileDisplay.js
@@ -5,25 +5,27 @@ function displayFiles() {
     const fileListContainer = document.getElementById('file-list');
 
     // Access files variable passed from render_template
-    const fileData = {{ files| tojson | safe
+    const fileData = {{ files | tojson | safe
 }};
 
-fileData.forEach(file => {
-    const fileDiv = document.createElement('div');
-    fileDiv.classList.add('file-item');
+if (fileListContainer) {
+    fileData.forEach(file => {
+        const fileDiv = document.createElement('div');
+        fileDiv.classList.add('file-item');
 
-    const iconImg = document.createElement('img');
-    iconImg.src = `{{ url_for('static', filename='images/') }}${file.icon}`;
-    iconImg.alt = 'Excel Icon';
+        const iconImg = document.createElement('img');
+        iconImg.src = `{{ url_for('static', filename='images/') }}${file.icon}`;
+        iconImg.alt = 'Excel Icon';
 
-    const fileName = document.createElement('p');
-    fileName.textContent = file.name;
+        const fileName = document.createElement('p');
+        fileName.textContent = file.name;
 
-    fileDiv.appendChild(iconImg);
-    fileDiv.appendChild(fileName);
+        fileDiv.appendChild(iconImg);
+        fileDiv.appendChild(fileName);
 
-    fileListContainer.appendChild(fileDiv);
-});
+        fileListContainer.appendChild(fileDiv);
+    });
+}
 }
 
 // Call the displayFiles function when the page loads
