f3af24f14a6b6aa9d98a008d455b99de8ae71fd9
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-09 15:23:35 +0200

    fixed uploading files added alert user and redirec renamed index to home

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, quote
 import os
 import shutil
 import tempfile
@@ -11,32 +11,48 @@ ALLOWED_EXTENSIONS: set = {".xlsx", ".csv"}
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
+def alert_user_and_redirect():
+    # Decorate a app.route decorated method with this to alert a user and redirect
+    # once it finishes. place this under the app.route decorator
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            redir, msg = func(*args, **kwargs)  # Must return redirect and message
+            return redirect(redir + "?notification=" + quote(msg))
+
+        return wrapper
+
+    return decorator
+
+
 def is_allowed_file(filename) -> bool:
     _, ext = os.path.splitext(filename)
     return ext.lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 
 @app.route("/upload", methods=["POST"])
-def upload_file():
+@alert_user_and_redirect()
+def upload_file() -> tuple[str, str]:
+    home_page = url_for("home")
+
     if "file" not in request.files:
-        return "No file in request"
+        return home_page, "No file in request"
 
     file = request.files["file"]
 
     if not file or file.filename == "":
-        return "No file selected"
+        return home_page, "No file selected"
 
-    if not is_allowed_file(file):
-        return "Invalid file type"
+    if not is_allowed_file(file.filename):
+        return home_page, "Invalid file type"
 
     file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
     file.save(file_path)
-    return "Uploaded file successfully"
+    return home_page, "Uploaded file successfully"
 
 
 @app.route("/process_files")
@@ -47,15 +63,17 @@ def show_files():
 
 
 @app.route("/discard_files")
-def discard_session():
+@alert_user_and_redirect()
+def discard_session() -> tuple[str, str]:
     global UPLOAD_FOLDER
+    home_page = url_for("home")
 
     # Remove all files from temp folder, start new session
     shutil.rmtree(app.config["UPLOAD_FOLDER"])
     UPLOAD_FOLDER = tempfile.mkdtemp()
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-    return "Session dropped, files removed from temporary folder"
+    return home_page, "Session dropped, files removed from temporary folder"
 
 
 if __name__ == "__main__":
