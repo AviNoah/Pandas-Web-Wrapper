cce694450ac193346ace3bf5f0b9ec8d8f576010
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-09 16:54:49 +0200

    using json.parse instead of jinja parsing, added comments

--- static/javascripts/fileDisplay.js
+++ static/javascripts/fileDisplay.js
@@ -2,30 +2,35 @@
 
 // Function to dynamically display files
 function displayFiles() {
+
+    // Get file list container
     const fileListContainer = document.getElementById('file-list');
 
     // Access files variable passed from render_template
-    const fileData = {{ files | tojson | safe
-}};
+    const fileData = JSON.parse(files)
 
-if (fileListContainer) {
-    fileData.forEach(file => {
-        const fileDiv = document.createElement('div');
-        fileDiv.classList.add('file-item');
+    if (fileListContainer) {
+        fileData.forEach(file => {
+            // The div to hold the image and file name text
+            const fileDiv = document.createElement('div');
+            fileDiv.classList.add('file-item');
 
-        const iconImg = document.createElement('img');
-        iconImg.src = `{{ url_for('static', filename='images/') }}${file.icon}`;
-        iconImg.alt = 'Excel Icon';
+            // Create image
+            const iconImg = document.createElement('img');
+            iconImg.src = `{{ url_for('static', filename='images/') }}${file.icon}`;
+            iconImg.alt = 'Excel Icon';
 
-        const fileName = document.createElement('p');
-        fileName.textContent = file.name;
+            // Create paragraph
+            const fileName = document.createElement('p');
+            fileName.textContent = file.name;
 
-        fileDiv.appendChild(iconImg);
-        fileDiv.appendChild(fileName);
+            // Append children
+            fileDiv.appendChild(iconImg);
+            fileDiv.appendChild(fileName);
 
-        fileListContainer.appendChild(fileDiv);
-    });
-}
+            fileListContainer.appendChild(fileDiv);
+        });
+    }
 }
 
 // Call the displayFiles function when the page loads
