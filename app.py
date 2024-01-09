462a52944f7dd03d23878ecf607ca69b45f89638
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-09 15:28:29 +0200

    home() now uses home.html

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def is_allowed_file(filename) -> bool:
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 @app.route("/upload", methods=["POST"])
