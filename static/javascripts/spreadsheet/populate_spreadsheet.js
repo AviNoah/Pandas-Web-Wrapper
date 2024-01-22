79c8a5b572f8bb1b9e6480301ca08e403964271d
Avi Noah <AviNoah2019@gmail.com>
2024-01-22 05:16:28 +0200

    Added updateSpreadSheetElement function and applyFilter function; added filter and header spreadsheet styles;

--- static/javascripts/spreadsheet/populate_spreadsheet.js
+++ static/javascripts/spreadsheet/populate_spreadsheet.js
@@ -1,3 +1,6 @@
+// Get reference to spreadsheet element div
+const spreadsheetElement = document.getElementById('spreadsheet');
+
 // Get reference to the selected sheet spinner element
 const selectedSheetSpinner = document.getElementById('sheetSelector');
 selectedSheetSpinner.addEventListener('change', changeSheet);
@@ -24,4 +27,46 @@ function changeSheet() {
         // Update the spreadsheet element
         updateSpreadsheetElement(sheet);
     }).catch(error => console.error("Error while parsing selected workbook :", error))
+}
+
+function updateSpreadsheetElement(sheet, editable = false) {
+    // Convert sheet data to HTML with grid lines
+    const html = XLSX.utils.sheet_to_html(sheet, { editable: editable, showGridLines: true });
+
+    // Display the HTML in the spreadsheet div
+    spreadsheetElement.innerHTML = html;
+
+    // Mark header rows as header-cell
+    const firstRowCells = spreadsheetElement.querySelectorAll('tr:first-child td');
+    firstRowCells.forEach(cell => {
+        cell.classList.add('header-cell');
+
+        // Create a wrapper div for proper spacing
+        const wrapperDiv = document.createElement('div');
+        wrapperDiv.classList.add('wrapper')
+
+        // Design header cell
+        const filterImg = document.createElement('img');
+        filterImg.src = '../static/images/filter_logo.svg';
+        filterImg.alt = 'Filter';
+        filterImg.classList.add("filter");
+
+
+        const cellName = document.createTextNode(cell.textContent);
+
+        wrapperDiv.appendChild(cellName);
+        wrapperDiv.appendChild(filterImg);
+
+        // Clear the original content and append the wrapper div
+        cell.innerHTML = '';
+        cell.appendChild(wrapperDiv);
+
+        // Apply filter when the filter image is clicked
+        filterImg.addEventListener('click', () => applyFilter(cell.cellIndex + 1)); // Add 1 to cellIndex to adjust for 0-based index
+    });
+}
+
+function applyFilter(column)
+{
+    // TODO: Implement this to POST the added filter to back-end.
 }
