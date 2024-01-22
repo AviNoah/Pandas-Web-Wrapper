44ebdda416df494b45127c118677d26afb2f6111
Avi Noah <AviNoah2019@gmail.com>
2024-01-22 03:40:37 +0200

    starting from scratch

--- static/javascripts/redirect_selected_file.js
+++
@@ -1,9 +0,0 @@
-function redirect_selection() {
-    // Get selected file
-    let selectedFile = document.querySelector('.file-item.selected-file');
-
-    if (selectedFile !== null)
-        window.location.href = `${home_folder}?selectedFile=${encodeURIComponent(selectedFile.textContent)}`;
-    else
-        window.location.href = home_folder;
-}
