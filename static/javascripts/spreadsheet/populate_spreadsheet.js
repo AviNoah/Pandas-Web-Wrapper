2c3d2409a70964e34980ae00d045bf837804707f
Avi Noah <AviNoah2019@gmail.com>
2024-01-22 04:29:34 +0200

    Added populate_spreadsheet js fiile

---
+++ static/javascripts/spreadsheet/populate_spreadsheet.js
@@ -0,0 +1,3 @@
+// Get reference to the selected sheet spinner element
+const selectedSheetSpinner = document.getElementById('selectedSheet');
+selectedSheetSpinner.addEventListener('change', changeSheet);
