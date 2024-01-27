6b0112bc2be792df1f52da479aa17a840331e2ce
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-27 16:20:14 +0200

    Added getFileView, renamed getFilename to getFileName

--- static/javascripts/select_file/file_view_handler.js
+++ static/javascripts/select_file/file_view_handler.js
@@ -6,12 +6,20 @@ function handleQueryList(event) { }
 
 function handleDownload(event) { }
 
-function handleDelete(event) { }
+function handleDelete(event) {
+    event.target.parentElement
+}
 
 
-function getFilename(view) {
+function getFileView(view) {
+    // Return file view from a member of the buttons container
+    const fileViewDiv = view.parentElement.parentElement
+    return fileViewDiv
+}
+
+function getFileName(view) {
     // View must be a member of buttons container
-    const filenameDiv = view.parentElement.querySelector('p.file-name');
+    const filenameDiv = getFileView(view).querySelector('p.file-name');
     const filename = filenameDiv.textContent;
 
     return filename
