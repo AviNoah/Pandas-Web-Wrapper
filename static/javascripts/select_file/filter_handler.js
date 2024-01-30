0efe11b4bb5db0bc4df293b44eae0daf415243ae
Avi Noah <AviNoah2019@gmail.com>
2024-01-30 02:23:07 +0200

    moved populateFilters to filter_handler; implemented handleFilterDelete

--- static/javascripts/select_file/filter_handler.js
+++ static/javascripts/select_file/filter_handler.js
@@ -3,6 +3,37 @@ function handleFilterSubmit(event) {
 }
 
 function handleFilterDelete(event) {
+    const filterItem = getFilterItem(event.target);
+
+    const choice = confirm(`Are you sure you want to remove this filter?`);
+    if (!choice)
+        return;  // User cancelled action
+
+    const filterID = filterItem.dataset.id;
+
+    const data = JSON.stringify({ filename: fileName }, { filter: filterID });
+
+    fetch('/filter/delete', {
+        method: "POST",
+        headers:
+        {
+            "Content-Type": "application/json",
+        },
+        body: data
+    })
+        .then(response => {
+            if (!response.ok)
+                throw new Error("Failed deleting filter");
+
+            return response.json();
+        })
+        .then(json => {
+            console.log(json["message"]);
+            filterItem.parentElement.removeChild(filterItem);  // Remove div
+        })
+        .catch(error => {
+            console.error(error);
+        })
 
 }
 
@@ -18,4 +49,49 @@ function getFilterContainer(buttonView) {
 function getFilterFileName(buttonView) {
     const filterContainer = getFilterContainer(buttonView);
     return filterContainer.dataset.filename;
+}
+
+
+function populateFilters(filters, fileName) {
+    // From the filters list return a div wrapper of the filters.
+    const filtersContainerDiv = document.createElement('div');
+    filtersContainerDiv.classList.add('filters-container');
+    filtersContainerDiv.setAttribute('data-filename', fileName);
+
+    return fetch('templates/select_file/filter_item.html')
+        .then(response => {
+            if (!response.ok)
+                throw new Error("Failed to retrieve filter item template");
+
+            return response.text();
+        })
+        .then(content => {
+            filters.forEach(filter => {
+                const filterItem = document.createElement('div');
+                filterItem.classList.add('filter-item');
+                filterItem.innerHTML = content;
+
+                const tableColumns = getTableColumns();
+
+                // Populate column selector
+                const columnSelect = filterItem.querySelector('select[name="column"]');
+                tableColumns.forEach(item => {
+                    const optionElement = document.createElement('option');
+                    optionElement.value = item;
+                    optionElement.textContent = item;
+                    columnSelect.appendChild(optionElement);
+                })
+                columnSelect.value = filter.column;
+
+                const methodSelect = filterItem.querySelector('select[name="method"]');
+                methodSelect.value = filter.method;
+
+                const inputField = filterItem.querySelector('input[name="input"]');
+                inputField.textContent = filter.input;
+
+                filtersContainerDiv.appendChild(filterItem);
+            })
+            return filtersContainerDiv
+        })
+        .catch(error => console.error(error));
 }
